eef0c3dbfcd471c87c8bad5ff64abb580d34305e
Ayonaire9090 <awosanya.ayobami41@gmail.com>
2026-05-12 18:44:46 +0100

    Increase the padding

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -22,7 +22,7 @@ export default function AuthLayout({
       <div className="relative z-10 flex flex-col justify-center items-center w-full min-h-screen px-4 sm:px-6 lg:px-8 py-8 lg:py-12">
         {/* Auth Logo */}
         <AuthLogo className="mb-0 lg:mb-3" />
-        <div className="w-full max-w-[98%] lg:max-w-[580px] bg-white rounded-[50px] shadow-2xl lg:px-10 lg:py-12 px-4 py-8">
+        <div className="w-full max-w-[99%] lg:max-w-[580px] bg-white rounded-[50px] shadow-2xl lg:px-10 lg:py-12 px-2 py-4">
           {children}
         </div>
       </div>
