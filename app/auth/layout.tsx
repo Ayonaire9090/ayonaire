5f381dfbdcf7e4b2fee18e9f345dea2ab8a0f4fd
Emmyjagz <praiseakomaye442@gmail.com>
2026-08-13 14:11:28 +0100

    Add auth card border treatment

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -24,7 +24,7 @@ export default function AuthLayout({
         <div className="relative z-10 flex flex-col justify-center items-center w-full min-h-screen px-5 sm:px-6 lg:px-8 py-8 lg:py-12">
           {/* Auth Logo */}
           <AuthLogo className="mb-5 lg:mb-8" />
-          <div className="w-full max-w-[382px] lg:max-w-[532px] bg-white rounded-[42px] lg:rounded-[52px] shadow-[0_12px_32px_rgba(18,19,21,0.14)] px-6 py-8 sm:px-8 lg:px-[46px] lg:py-10">
+          <div className="w-full max-w-[382px] lg:max-w-[532px] bg-white rounded-[42px] lg:rounded-[52px] border border-[#F1F1F1] shadow-[0_14px_28px_rgba(18,19,21,0.12)] px-6 py-8 sm:px-8 lg:px-[46px] lg:py-10">
             {children}
           </div>
         </div>
